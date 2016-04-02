b74fd0f856d30b99e62907538544809796edc174
dikou <Diana@0587383439.wireless.umich.net>
2016-04-02 01:15:26 -0400

    Fix nav for id "currentpage"

--- main.py
+++ main.py
@@ -31,15 +31,8 @@ class ContactHandler(webapp2.RequestHandler):
         self.response.write(template.render({'title': 'CONTACT'}))
 
     def post(self):
-        user = self.request.get('username')
-        pw = self.request.get('password')
-        if pw == 'pass' and user == 'Colleen':
-            template = JINJA_ENVIRONMENT.get_template('templates%s' % '/contact')
-            self.response.write(template.render({'title': 'CONTACT'}))
-        else:
-            logging.info('Invalid credentials: user='+user+' pw='+pw)
-            template = JINJA_ENVIRONMENT.get_template('templates%s' % '/form')
-            self.response.write(template.render({'title': 'CONTACT'}))
+        template = JINJA_ENVIRONMENT.get_template('templates%s' % '/contact')
+        self.response.write(template.render({'title': 'CONTACT'}))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
